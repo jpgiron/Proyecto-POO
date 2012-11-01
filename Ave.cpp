df76720a8c45f54cb5dfa901dbb0a63787172473
Juan Pablo Giron Ruiz <gironjuanpablo@hotmail.com>
2012-10-31 21:53:35 -0500

    Se contruyo la clase Ave

--- Ave.cpp
+++ Ave.cpp
@@ -1,9 +1,39 @@
 #include "Ave.h"
 
-Ave::Ave(void)
-{
-}
+#define Radio 50
 
 Ave::~Ave(void)
 {
+	
+}
+
+float Ave::calculoVelocidad()
+{
+	int tmp;
+	tmp=sqrt(pow((double) posx-PosXo,2)+pow((double) posy-PosYo,2));
+	return (tmp*120/Radio);
+
+}
+
+float Ave::calculoAngulo()
+{
+	Angle=abs(dbSpriteAngle(ID));//En grados;
+	return (Angle*3.1415)/180;
+}
+
+void Ave::moveBird(){
+
+	dbSprite(ID,posx,posy,ID_Image);
+	dbOffsetSprite(ID,dbSpriteWidth(ID)/2,dbSpriteHeight(ID)/2);
+	dbSync();
+}
+
+void Ave::rotateBird()
+{
+	dbRotateSprite(ID,dbSpriteAngle(ID)-1);
+}
+
+float Ave::RetornarAngulo()
+{
+	return Angle;
 }
--- Ave.h
+++ Ave.h
@@ -4,8 +4,22 @@
 
 class Ave:public figura
 {
+private:
+	int pixel;
+	float Angle;
+	int PosXo;
+	int PosYo;
 public:
-	Ave(void);
+	Ave(int ID_Im,int ID_Sprite,int posx,int posy,string Identi):figura(ID_Im,ID_Sprite,posx,posy,Identi)
+	{
+		pixel=0;
+		PosXo=300;  //Centro en X del circulo de radio 50
+		PosYo=530;  //Centro en Y del circulo de radio 50
+	}
 	~Ave(void);
-
+	void  moveBird();
+	void  rotateBird();
+	float calculoAngulo();
+	float calculoVelocidad();
+	float RetornarAngulo();
 };
