7d5373c35d103056cfdafe06f6ccf5c2988c5e08
Juan Pablo Giron Ruiz <gironjuanpablo@hotmail.com>
2012-11-02 15:26:13 -0500

    Se modifico los atrubutos a la clase Ave, quitanto pixel y PosXo y posYo

--- Ave.h
+++ Ave.h
@@ -1,20 +1,26 @@
 #pragma once
 
 #include "figura.h"
+#include "darkgdk.h"
+#include <cmath>
+
+#define PosXo=300;  //Centro en X del circulo de radio 50
+#define	PosYo=530;  //Centro en Y del circulo de radio 50 
+
+using namespace std;
+
 
 class Ave:public figura
 {
 private:
-	int pixel;
+
 	float Angle;
-	int PosXo;
-	int PosYo;
+
 public:
 	Ave(int ID_Im,int ID_Sprite,int posx,int posy,string Identi):figura(ID_Im,ID_Sprite,posx,posy,Identi)
 	{
-		pixel=0;
-		PosXo=300;  //Centro en X del circulo de radio 50
-		PosYo=530;  //Centro en Y del circulo de radio 50
+		
+		
 	}
 	~Ave(void);
 	void  moveBird();
